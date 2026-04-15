092d67eae4e09018f2c1134ea9b4d4746b7977f8
TomasOnesti <tomas.onesti.t1vl@gmail.com>
2026-04-15 12:07:55 -0300

    base del inicio de seion

---
+++ paginas/inicio_sesion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marketing</title>
+</head>
+<body>
+    <header>
+        <div></div>
+        <div></div>
+    </header>
+    <main>
+        <form action="inicio.php" method="post">
+            <label for="nombre">Nombre: </label>
+            <input type="text" name="nombre" id= "nombre" placeholder="Escribe tu nombre de usuario">
+
+            <label for="email">Email: </label>
+            <input type="email" id="email" name="email" placeholder="Escribe tu correo electronico">
+
+            <label for="password">Contraseña: </label>
+            <input type="password" id="password" name="password" placeholder="Contraseña">
+        </form>
+    </main>
+    <footer>
+        <p></p>
+    </footer>
+</body>
+</html>

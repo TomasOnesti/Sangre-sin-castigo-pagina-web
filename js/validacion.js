8a0a7f4d7de405acc4cd87a6945fb5c29606ac9d
TomasOnesti <tomas.onesti.t1vl@gmail.com>
2026-04-25 23:02:11 -0300

    validacion js

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,8 @@
-document.getElementById("formulario").addEventListener("submit", function(e){
-    let campos = document.querySelectorAll("#formulario input");
+document.getElementById("formulario").addEventListener("submit", function(event){
+    let campos = document.querySelectorAll("#formulario .input");
     let valido = true;
-    
+    let pass = document.getElementById("password");
+    let repass = document.getElementById("repassword");
     campos.forEach(espacio => {
         if (espacio.value.trim() === "") {
             valido = false;
@@ -10,4 +11,33 @@ document.getElementById("formulario").addEventListener("submit", function(e){
             espacio.classList.add("validado");
         }
     });
+    if(!valido){
+        event.preventDefault();
+        Toastify({
+            text: "Escribe en todos los campos",
+            duration: 3000,
+            gravity: "top",
+            position:"right",
+            className: "alert",
+            style: {
+                background:"linear-gradient(to right, #f52323, #c7c93d)"
+            }
+        }).showToast();
+    }
+
+    if(pass !== repass){
+        valido = false;
+        event.preventDefault();
+        Toastify({
+            text: "Las contraseñas no coinciden",
+            duration: 3000,
+            gravity: "top",
+            position:"right",
+            className: "alert",
+            style: {
+                background:"linear-gradient(to right, #f52323, #c7c93d)"
+            }
+        }).showToast();
+    }
+
 });
